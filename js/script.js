ca6ebc5ae9aae7b973ee7dfccdf39886bec85cd4
COPSTA <tutomx16@gmail.com>
2024-03-28 13:41:08 -0500

    Se crea la funcion para mostrar las lista de canciones de forma ordenada alfabeticamente

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
-
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
@@ -82,17 +81,58 @@ const allSongs = [
 ];
 const audio = new Audio(); //Se crea un objecto de Audio que a su ves es una (API) navita de js
 let userData = {
-    songs: [...allSongs], //el operador ...sir
-    currentSong:null,
-    songCurrentTime:0,
+  songs: [...allSongs], //el operador ...sir
+  currentSong: null,
+  songCurrentTime: 0,
 };
 
 /**FUNCIONES */
-
+/**
+ * Muestra la lista de canciones 
+ */
 const renderSongs = (array) => {
-    const songsHTML = array.map((song) => {return 
-        `<li id="song-${song.id}" class="playlist-song">
-        
-        </li>`} 
-        ); //El método map() se utiliza para iterar a través de una matriz y devolver una nueva matriz.
-}
+  const songsHTML = array
+    .map((song) => {//El método map() se utiliza para iterar a través de una matriz y devolver una nueva matriz.
+    return`
+      <li id="song-${song.id}" class="playlist-song">
+      <button class="playlist-song-info">
+          <span class="playlist-song-title">${song.title}</span>
+          <span class="playlist-song-artist">${song.artist}</span>
+          <span class="playlist-song-duration">${song.duration}</span>
+      </button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
+          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
+        </button>
+      </li>
+      `;
+      })
+      .join(""); //join() Se utiliza para concatenar todos los elementos de una matriz en una sola cadena
+  
+      playlistSongs.innerHTML = songsHTML;
+
+};
+
+/**
+ * ORDERNA LA LISTA DE CACIONES DE FORMA ALFABETICA
+ * El método sort() convierte elementos de una matriz 
+ * en cadenas y los ordena según sus valores en la codificación UTF-16.
+ */
+const sortSongs = () =>{
+  userData?.songs.sort(
+    (a,b)=>{
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+  });
+  return userData?.songs;
+};
+
+
+
+
+renderSongs(sortSongs(userData?.songs));
