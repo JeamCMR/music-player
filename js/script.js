5f1b36e348301a98d903da1962192fd97c2298b8
COPSTA <tutomx16@gmail.com>
2024-03-30 17:53:37 -0500

    Se agrega la funciones de eliminar canciones y reproducir cancion siguiente automaticamente

--- js/script.js
+++ js/script.js
@@ -178,6 +178,41 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 }
 
+
+
+/**Funcion para eliminar canciones */
+
+const deleteSong = (id) =>{
+  userData.songs = userData?.songs.filter((song)=>  song.id !== id );
+  renderSongs(userData?.songs);
+  highlightCurrentSong();
+  setPlayButtonAccessibleText();
+  // ser valida si la cancion a eliminar es la que se reproduce actualmente
+  if(userData?.currentSong?.id === id){ 
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+  }
+  
+  //Si la lista de canciones esta vacia se debe crear un botton para reiniar.
+  if(userData?.songs.length === 0){
+    const resetButton = document.createElement("button"); //createElement se puede crear etiquetas html
+    const resetText = document.createTextNode("Reset Playlist"); // crear un nodo de texto para el boton
+    resetButton.id="reset"; //Agregar un atributo id al button creado
+    resetButton.ariaLabel="Reset playlist" //Agregar un atributo ariaLabel al button creado
+    resetButton.appendChild(resetText); // Agrega el texto al boton
+    playlistSongs.appendChild(resetButton); //agrega el boton a la lista de reproduccion como elemento segundario
+
+    resetButton.addEventListener("click", ()=>{
+      userData.songs = [...allSongs];
+      renderSongs(sortSongs());
+      setPlayButtonAccessibleText()
+      resetButton.remove();
+    })
+  }
+}
+
 /**
  * Muestra la lista de canciones 
  */
@@ -185,18 +220,18 @@ const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {//El método map() se utiliza para iterar a través de una matriz y devolver una nueva matriz.
     return`
-      <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info" onclick="playSong(${song.id})">
-          <span class="playlist-song-title">${song.title}</span>
-          <span class="playlist-song-artist">${song.artist}</span>
-          <span class="playlist-song-duration">${song.duration}</span>
+    <li id="song-${song.id}" class="playlist-song">
+    <button class="playlist-song-info" onclick="playSong(${song.id})">
+        <span class="playlist-song-title">${song.title}</span>
+        <span class="playlist-song-artist">${song.artist}</span>
+        <span class="playlist-song-duration">${song.duration}</span>
+    </button>
+    <button onclick="deleteSong(${song.id})" class="playlist-song-delete" aria-label="Delete ${song.title}">
+        <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
       </button>
-      <button class="playlist-song-delete" aria-label="Delete ${song.title}">
-          <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/>
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg>
-        </button>
-      </li>
-      `;
+    </li>
+    `;
       })
       .join(""); //join() Se utiliza para concatenar todos los elementos de una matriz en una sola cadena
   
@@ -243,6 +278,24 @@ previousButton.addEventListener("click", playPreviousSong);
 /**Se agrega la funcion de aleatorizar las canciones  al button shuffleButton  */
 
 shuffleButton.addEventListener("click", shuffle);
+
+
+/**Reproducir siguiente cancion automaticamente cuando la anterior acabe */
+audio.addEventListener("ended", ()=>{
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData.songs.length > currentSongIndex ? true : false ; //Se valida si el valor de matriz.length es mator que currentSongIndex
+  if (nextSongExists){ //si existe
+    playNextSong();
+  }else{
+    userData.currentSong= null;
+    userData.songCurrentTime= 0;
+    pauseSong();
+    setPlayerDisplay();
+    highlightCurrentSong();
+    setPlayButtonAccessibleText();
+  }
+})
+
 
 /**
  * ORDERNA LA LISTA DE CACIONES DE FORMA ALFABETICA
